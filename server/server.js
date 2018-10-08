5366538f764c550ff0c54e5510decfcd6886aa2e
punith <akshay0696@gmail.com>
2018-10-08 14:39:08 +0530

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 "use strict";
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser'); // Takes the JSON response converts it to object and send to server
